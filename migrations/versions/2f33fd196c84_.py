3dbb555e6e2b30d1f2442df31a8ad2388bb41ce4
Adrianmrc94 <adrianmrc94@gmail.com>
2025-06-05 01:33:43 +0000

    segunda prueba

--- migrations/versions/2f33fd196c84_.py
+++ migrations/versions/2f33fd196c84_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 3bf7d554987b
+Revision ID: 2f33fd196c84
 Revises: 
-Create Date: 2025-06-05 01:27:13.451603
+Create Date: 2025-06-05 01:32:59.652583
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '3bf7d554987b'
+revision = '2f33fd196c84'
 down_revision = None
 branch_labels = None
 depends_on = None
